561d7f807abaa833da74d4ed7c1914296ef0c1ec
PRABHVEERSINGH17 <146875097+PRABHVEERSINGH17@users.noreply.github.com>
2024-05-09 14:11:07 +0530

    Add files via upload

---
+++ .c
@@ -0,0 +1,12 @@
+// A tree is a non linear data structutre which represent and organise data  which is easy to search and navigate.
+//node is defined as the elements of the tree.(basic unit)/*
+//edge is defined as the connection between two nodes//
+//root is the top most node of the tree.
+//parent node is defined as the imediate predeccsors of the node.
+//child node is defined as the imediate successors of the node.
+// dgree of the node is defined as the total number of childhren contained by parent node.
+//dgree of a tree is defined as the highest dgree of amy node.
+//terminal node is defined as the maximum no of nodes from leaf node to that node.
+//siblings is defined as the 
+ //height of a node is the number of edges present in the longest path connecting that node to a leaf node.
+ //binary tree is defined as the tree which contains only two childhren which are known as left child and right child.
